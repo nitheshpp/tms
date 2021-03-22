cd37a744b3038b46f22f76d7a51ddf2a99aaed3d
dell <dell@DESKTOP-ILHQECJ>
2021-03-22 16:23:05 +0530

    new commit(Controller and exception)

--- src/main/java/tms/controller/TMSController.java
+++ src/main/java/tms/controller/TMSController.java
@@ -1,70 +1,136 @@
 package tms.controller;
 
+import java.util.HashMap;
 import java.util.List;
-
-
-
+import java.util.Map;
 
 import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import tms.dao.TaskListItemsRepository;
 import tms.dao.TaskListUserRepository;
+import tms.exception.ResourceNotFoundException;
 import tms.model.CResultListItems;
 import tms.model.CResultTaskListUser;
 import tms.model.TaskListItems;
 import tms.model.TaskListUser;
 import tms.service.TmsServiceInterface;
-
+@CrossOrigin(origins = "http://localhost:3000")
 
 @RestController
+@RequestMapping("/api/")
 public class TMSController {
 	
 	@Autowired
 	private TmsServiceInterface s;
 	
+	@Autowired
+	private TaskListUserRepository tasklistuserRepository;
 	
+	@Autowired
+	private TaskListItemsRepository tasklistitemRepository;
 	
-	@GetMapping("/getUser")
+	@GetMapping("/users")
+	public List<TaskListUser> getUsers(){
+		return tasklistuserRepository.findAll();
+	}
+	
+	// get user by id rest api
+		@GetMapping("/users/{id}")
+		public ResponseEntity<TaskListUser> getUserById(@PathVariable Long id) {
+			TaskListUser user = tasklistuserRepository.findById(id)
+					.orElseThrow(() -> new ResourceNotFoundException("user not exist with id :" + id));
+			return ResponseEntity.ok(user);
+		}
+	
+	/*@GetMapping("/getUser")
 	public CResultTaskListUser getUser(@RequestParam("x") long id)
 	{
 		
 		 CResultTaskListUser x = s.getUser(id);
 		return x;
-	}
+	}*/
 	
+	// create user rest api
+		@PostMapping("/users")
+		public TaskListUser createUser(@RequestBody TaskListUser user) {
+			return tasklistuserRepository.save(user);
+		}
 	
-	
-   @PutMapping("/addUser")
+  /* @PutMapping("/addUser")
 	
 	public CResultTaskListUser addEditUser(@RequestBody TaskListUser c){
 	   System.out.println("inside add");
 	   CResultTaskListUser x = s.addUser(c);
 		return x;
-	}
+	}*/
    
-   @PutMapping("/updateUser")
+// update user rest api
+	
+	@PutMapping("/users/{id}")
+	public ResponseEntity<TaskListUser> updateUser(@PathVariable Long id, @RequestBody TaskListUser userDetails){
+		TaskListUser user = tasklistuserRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("user not exist with id :" + id));
+		
+		user.setUserName(userDetails.getUserName());
+		user.setPasswordHash(userDetails.getPasswordHash());
+		user.setMobileNo(userDetails.getMobileNo());
+		user.setManager(userDetails.isManager());
+		user.setAdministrator(userDetails.isAdministrator());
+		
+		TaskListUser updatedUser = tasklistuserRepository.save(user);
+		return ResponseEntity.ok(updatedUser);
+	}
+	
+	// delete users rest api
+		@DeleteMapping("/users/{id}")
+		public ResponseEntity<Map<String, Boolean>> deleteUser(@PathVariable Long id){
+			TaskListUser user = tasklistuserRepository.findById(id)
+					.orElseThrow(() -> new ResourceNotFoundException("User not exist with id :" + id));
+			
+			tasklistuserRepository.delete(user);
+			Map<String, Boolean> response = new HashMap<>();
+			response.put("deleted", Boolean.TRUE);
+			return ResponseEntity.ok(response);
+		}
+   
+  /* @PutMapping("/updateUser")
 	public CResultTaskListUser editUser(@RequestBody TaskListUser u){
 		
 	   System.out.println("inside update");
 	   CResultTaskListUser x = s.updateUser(u);
 		return x;
-	}
+	}*/
    
-   @PutMapping("/createTask")
+// create user rest api
+		@PostMapping("/tasks")
+		public TaskListItems createTask(@RequestBody TaskListItems user) {
+			return tasklistitemRepository.save(user);
+		}
+   
+		@GetMapping("/tasks")
+		public List<TaskListItems> getTasks(){
+			return tasklistitemRepository.findAll();
+		}
+		
+		
+  /* @PutMapping("/createTask")
 	public CResultListItems CreateTask(@RequestBody TaskListItems c){
 	   System.out.println("inside create");
 	   CResultListItems x=s.createTask(c);
 		return x;
-	}
+	}*/
 	
 	/******************************************************* nithesh **********************************************/
 	
